4c76fe2b9c13716ac1a0b51287725f1665565587
Nyota Mwangi <nyota.benjamin@gmail.com>
2020-01-27 02:56:23 +0300

    Create test fixture and initial test case

--- test_locker.py
+++ test_locker.py
@@ -4,14 +4,25 @@ from locker import Credential, User
 
 
 class TestUser(unittest.TestCase):
-    """Test class that defines test cases for the User class
+    """Test class that defines test cases for the User class.
     """
 
-    pass
+    def setUp(self):
+        """Set up method to run before each test case."""
+        self.new_user = User("trucktar", "TruckOnTAR")
+
+    def test_create_user(self):
+        """Test case to test if user is created properly."""
+        self.assertEqual(self.new_user.username, "trucktar")
+        self.assertEqual(self.new_user.password, "TruckOnTAR")
 
 
 class TestCredential(unittest.TestCase):
-    """Test class that defines test cases for the Credential class
+    """Test class that defines test cases for the Credential class.
     """
 
     pass
+
+
+if __name__ == "__main__":
+    unittest.main()
