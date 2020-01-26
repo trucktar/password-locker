909f39ed9909b885544eab4958e1f57fdb4bfd6e
Nyota Mwangi <nyota.benjamin@gmail.com>
2020-01-27 02:04:47 +0300

    Create User and Credential classes plus test suites

---
+++ locker.py
@@ -0,0 +1,12 @@
+class User:
+    """Class that generates instances of users
+    """
+
+    pass
+
+
+class Credential:
+    """Class that generates instances of credentials
+    """
+
+    pass
---
+++ test_locker.py
@@ -0,0 +1,17 @@
+import unittest
+
+from locker import Credential, User
+
+
+class TestUser(unittest.TestCase):
+    """Test class that defines test cases for the User class
+    """
+
+    pass
+
+
+class TestCredential(unittest.TestCase):
+    """Test class that defines test cases for the Credential class
+    """
+
+    pass
