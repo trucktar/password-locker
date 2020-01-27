b458b67d0f5dcfd44f646edc73f19fd5a7138092
Nyota Mwangi <nyota.benjamin@gmail.com>
2020-01-27 04:21:02 +0300

    Add methods to create and save user instances

--- locker.py
+++ locker.py
@@ -2,7 +2,23 @@ class User:
     """Class that generates instances of users
     """
 
-    pass
+    user_list = []  # Empty users list
+
+    def __init__(self, username, password):
+        """Create user attributes after instantiation.
+
+        Args:
+            username: New user's username.
+            password: New user's password. 
+        """
+
+        self.username = username
+        self.password = password
+
+    def save_user(self):
+        """Append user instance to user_list."""
+
+        User.user_list.append(self)
 
 
 class Credential:
