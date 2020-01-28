672aea80cdd292c7a4e13bcaca2cc108daf4e109
Nyota Mwangi <nyota.benjamin@gmail.com>
2020-01-28 13:10:26 +0300

    Create required methods to make the tests pass

--- locker.py
+++ locker.py
@@ -18,6 +18,10 @@ class UserAccount:
         """Add new account instance to user_list."""
         UserAccount.user_list.append(self)
 
+    def delete_account(self):
+        """Remove an account instance from user_list."""
+        UserAccount.user_list.remove(self)
+
     @classmethod
     def login_account(cls, account):
         """Login to a specific account.
@@ -26,6 +30,20 @@ class UserAccount:
             account: Locker account to login
         """
         cls.active_user = account
+
+    @classmethod
+    def find_account_by_username(cls, username):
+        """Finds a locker account that matches a username
+        
+        Args:
+            username: Account username to use as filter
+        Returns:
+            UserAccount instance
+        """
+        user_match = filter(lambda user: user.username == username, cls.user_list)
+        found_user = next(user_match, None)
+
+        return found_user
 
 
 class Credential:
